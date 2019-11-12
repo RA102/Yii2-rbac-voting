8d319cfb81c85367afe547faf9f4123a8f9efcb6
RA <ra_lebedev@mail.ru>
2019-11-12 13:45:42 +0600

    folder models in modules

---
+++ modules/voting/models/MemberSearch.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace app\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Member;
+
+/**
+ * MemberSearch represents the model behind the search form of `app\models\Member`.
+ */
+class MemberSearch extends Member
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'active', 'status_student_id'], 'integer'],
+            [['name', 'faculty', 'department', 'specialty', 'theme', 'data'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        if (\Yii::$app->user->identity)
+
+        $query = Member::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'active' => $this->active,
+            'status_student_id' => $this->status_student_id,
+            'data' => $this->data,  //\Yii::$app->formatter->asDate($this->data, 'php: j.n.Y'),
+        ]);
+
+        $query->andFilterWhere(['like', 'name', $this->name])
+            ->andFilterWhere(['like', 'faculty', $this->faculty])
+            ->andFilterWhere(['like', 'department', $this->department])
+            ->andFilterWhere(['like', 'specialty', $this->specialty])
+            ->andFilterWhere(['like', 'theme', $this->theme]);
+
+        return $dataProvider;
+    }
+}
---
+++ modules/voting/models/Result.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "result".
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $member_id
+ * @property int $result_id
+ * @property int $type_id
+ * @property int $status_student_id
+ * @property int $active
+ */
+class Result extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'result';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'member_id'], 'required'],
+            [['user_id', 'member_id', 'result_id', 'type_id', 'status_student_id', 'active'], 'integer'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'member_id' => 'Member ID',
+            'result_id' => 'Result ID',
+            'type_id' => 'Type ID',
+            'status_student_id' => 'Status Student ID',
+            'active' => 'Active',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getMember()
+    {
+        return $this->hasOne(Member::className(), ['id' => 'member_id']);
+    }
+    public function getUsers()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+    public function getType()
+    {
+        return $this->hasOne(Type::className(), ['id' => 'type_id']);
+    }
+    public function getStatusStudent()
+    {
+        return $this->hasOne(StatusStudent::className(), ['id' => 'status_student_id']);
+    }
+
+}
